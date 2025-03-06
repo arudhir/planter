06e04b888061251e7f468619315db817b0b84d01
Ubuntu <ubuntu@ip-172-31-26-118.us-east-2.compute.internal>
2025-03-06 17:04:26 +0000

    removed weird patch thing

--- patch_and_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Apply the patch and run Snakemake
-
-# Apply the monkey patch to the Snakemake logger
-cd /usr/src/planter
-python patches/monkey_patch.py
-
-# Run the command that was passed to this script
-exec "$@"
